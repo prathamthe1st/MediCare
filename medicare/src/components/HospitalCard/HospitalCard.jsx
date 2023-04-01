7361ba52a8f3cdba16b0fdcbdf59954eacd595f7
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2023-04-01 12:16:49 +0530

    Inital Commit with Project Architecture

---
+++ medicare/src/components/HospitalCard/HospitalCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HospitalCard() {
+    return (
+        <div>HospitalCard</div>
+    )
+}
+
+export default HospitalCard
---
+++ medicare/src/pages/BloodBank/BloodBank.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BloodBank() {
+    return (
+        <div>BloodBank</div>
+    )
+}
+
+export default BloodBank
